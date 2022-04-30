827dee83315016b4c122b56b4d6351c810ecab79
Jose Daniel Posso Garcia <daniel7byte@gmail.com>
2022-04-29 21:20:09 -0500

    Outlet Demo 2

--- lib/src/pages/outlet_page.dart
+++ lib/src/pages/outlet_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/src/widgets/BottomNavBarRaisedInsetFb1.dart';
 
 class OutletPage extends StatelessWidget {
   const OutletPage({Key? key}) : super(key: key);
@@ -6,6 +7,7 @@ class OutletPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBody: true,
       appBar: AppBar(
         elevation: 0.5,
         backgroundColor: Colors.white,
@@ -46,71 +48,107 @@ class OutletPage extends StatelessWidget {
       body: Column(
         children: [
           Container(
+            width: double.infinity,
+            height: 60,
             padding: const EdgeInsets.only(
-              left: 25,
-              right: 25,
+              left: 20,
               top: 5,
             ),
-            child: Row(
+            child: ListView(
+              scrollDirection: Axis.horizontal,
               children: [
                 const Icon(Icons.filter_list),
                 buttonTags(text: 'ROPA'),
                 buttonTags(text: 'CALZADO'),
                 buttonTags(text: 'ACCESORIOS'),
+                buttonTags(text: 'COSMETICOS'),
+                buttonTags(text: 'INTERIORES'),
               ],
             ),
           ),
-          Container(
-            padding: const EdgeInsets.only(
-              left: 15,
-              right: 15,
-              top: 5,
-            ),
-            child: Row(
-              children: [
-                Card(
-                  child: InkWell(
-                    splashColor: Colors.blue.withAlpha(30),
-                    onTap: () {
-                      debugPrint('Card tapped.');
-                    },
-                    child: SizedBox(
-                      width: 190,
-                      height: 250,
-                      child: Column(
-                        children: [
-                          Image.network(
-                            'https://rukminim2.flixcart.com/image/332/398/kyvvtzk0/shoe/5/q/w/9-500-corsac-black-original-imagbyzezm5vkhyf.jpeg?q=50',
-                          ),
-                          Text('A card that can be tapped'),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                Card(
-                  child: InkWell(
-                    splashColor: Colors.blue.withAlpha(30),
-                    onTap: () {
-                      debugPrint('Card tapped.');
-                    },
-                    child: SizedBox(
-                      width: 190,
-                      height: 250,
-                      child: Column(
-                        children: [
-                          Image.network(
-                            'https://rukminim2.flixcart.com/image/332/398/kyvvtzk0/shoe/5/q/w/9-500-corsac-black-original-imagbyzezm5vkhyf.jpeg?q=50',
-                          ),
-                          Text('A card that can be tapped'),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ],
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(5),
+              child: GridView.count(
+                crossAxisCount: 1,
+                children: [
+                  productCard(),
+                  productCard(),
+                  // productCard(),
+                  // productCard(),
+                  // productCard(),
+                ],
+              ),
             ),
           )
+        ],
+      ),
+      bottomNavigationBar: const BottomNavBarRaisedInsetFb1(),
+    );
+  }
+
+  Widget bottomNavigationBar() {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.search),
+          label: 'Search',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.favorite),
+          label: 'Favorite',
+        ),
+        // BottomNavigationBarItem(
+        //   icon: Icon(Icons.shopping_cart_rounded),
+        //   label: 'Shop',
+        // ),
+        // BottomNavigationBarItem(
+        //   icon: Icon(Icons.radar),
+        //   label: 'Radar',
+        // ),
+      ],
+      // currentIndex: () {},
+      selectedItemColor: Colors.grey[800],
+      // onTap: () {},
+    );
+  }
+
+  Widget productCard() {
+    return Card(
+      child: Column(
+        children: [
+          ListTile(
+            title: Text('Camiseta'),
+            subtitle: Text('Black'),
+            trailing: Icon(Icons.favorite_outline),
+          ),
+          Expanded(
+            child: Image.asset(
+              'assets/img/camiseta.jpg',
+              fit: BoxFit.fitWidth,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(16.0),
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit dolorem ipsum quia dolor sit amet, consectetur, adipisci veli...',
+            ),
+          ),
+          ButtonBar(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.add_shopping_cart,
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
